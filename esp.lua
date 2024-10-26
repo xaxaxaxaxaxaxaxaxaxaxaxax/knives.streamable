92fca4240736775a2294547df415f6364df8b5c6
aladin <147218564+xaxaxaxaxaxaxaxaxaxaxaxax@users.noreply.github.com>
2024-10-26 19:48:30 -0400

    Update esp.lua

--- esp.lua
+++ esp.lua
@@ -1,9 +1,4 @@
--- local Settings = {
---    Box_Color = Color3.fromRGB(255, 255, 255),
---    Box_Thickness = 2,
- --   Team_Check = false,
- --   Team_Color = false
--- }
+
 
 
 local Space = game:GetService("Workspace")
